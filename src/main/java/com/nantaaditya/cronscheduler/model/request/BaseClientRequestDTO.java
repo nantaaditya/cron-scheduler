2099abcf5bbe9031fb7315c457bbc9665a933e51
pramuditya <pramuditya@alto.id>
2023-11-29 15:26:27 +0700

    update model equals and hash code call super

--- src/main/java/com/nantaaditya/cronscheduler/model/request/BaseClientRequestDTO.java
+++ src/main/java/com/nantaaditya/cronscheduler/model/request/BaseClientRequestDTO.java
@@ -1,23 +1,32 @@
 package com.nantaaditya.cronscheduler.model.request;
 
 import com.nantaaditya.cronscheduler.validation.HttpMethodMustValid;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.validator.constraints.URL;
 
 @Data
+@SuperBuilder
 @NoArgsConstructor
+@AllArgsConstructor
 public class BaseClientRequestDTO {
   @NotNull(message = "NotNull")
   @HttpMethodMustValid
+  @Schema(type = "string", allowableValues = {"POST", "PUT", "GET", "DELETE"})
   private String httpMethod;
   @NotNull(message = "NotNull")
   @URL(message = "MustValid")
+  @Size(max = 255, message = "TooLong")
   private String baseUrl;
   @NotNull(message = "NotNull")
+  @Size(max = 255, message = "TooLong")
   private String apiPath;
   private Map<String, String> pathParams;
   private Map<String, String> queryParams;
--- src/main/java/com/nantaaditya/cronscheduler/model/request/BaseJobExecutorRequestDTO.java
+++ src/main/java/com/nantaaditya/cronscheduler/model/request/BaseJobExecutorRequestDTO.java
@@ -2,11 +2,15 @@ package com.nantaaditya.cronscheduler.model.request;
 
 import com.nantaaditya.cronscheduler.validation.CronMustValid;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
 @NoArgsConstructor
+@AllArgsConstructor
 public class BaseJobExecutorRequestDTO {
   @NotNull(message = "NotNull")
   private String clientId;
